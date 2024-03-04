4a3c56162db441cf81f71b642754daa356631b09
Hridoy Saha <hridoysaha969@gmail.com>
2024-03-04 20:46:33 +0600

    version update 2.0.9

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,29 @@
 import Banner from "@/components/Banner";
+import dynamic from "next/dynamic";
+import classes from "@/styles/countdown.module.css";
+import { ArrowForward } from "@mui/icons-material";
+
+const DynamicCountdownTimer = dynamic(
+  () => import("../components/CountdownTimer"),
+  { ssr: false }
+);
+
 import ImageSlider from "@/components/ImageSlider";
 import Reward from "@/components/Reward";
 import Students from "@/components/Student";
+import Link from "next/link";
 
 export default function Home() {
+  const targetDate = new Date("2024-03-12T23:59:59");
   return (
     <div className="container">
+      <div className={classes.timer__wrapper}>
+        <h3 className={classes.timer__title}>Registration is in progress</h3>
+        <DynamicCountdownTimer targetDate={targetDate} />
+        <Link href="/registration" className={classes.timer__link}>
+          Apply Now <ArrowForward />
+        </Link>
+      </div>
       <Banner />
       <ImageSlider />
       <Reward />
---
+++ src/components/CountdownTimer.js
@@ -0,0 +1,57 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import classes from "@/styles/countdown.module.css";
+
+const CountdownTimer = ({ targetDate }) => {
+  const calculateTimeLeft = () => {
+    const difference = +new Date(targetDate) - +new Date();
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  });
+
+  const timerComponents = [];
+
+  Object.keys(timeLeft).forEach((interval) => {
+    if (!timeLeft[interval]) {
+      return;
+    }
+
+    timerComponents.push(
+      <div key={interval} className={classes.timer}>
+        <span>{timeLeft[interval]}</span> <span>{interval.charAt(0)}</span>{" "}
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      {timerComponents.length ? (
+        timerComponents
+      ) : (
+        <span>Registration Finished</span>
+      )}
+    </div>
+  );
+};
+
+export default CountdownTimer;
